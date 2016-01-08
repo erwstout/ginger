23b673c5b60524464d1cd13af5536661a0beb467
Eric Stout <eric@factor1studios.com>
2016-01-07 18:32:51 -0700

    Adds gulp-uglify and JS to allow for flex-basis adjustments

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
 
 // Compile Sass
 gulp.task('sass', function(){
@@ -26,6 +27,16 @@ gulp.task('cssnano', ['sass'], function(){
     .pipe(cssnano({
       discardComments: {removeAllButFirst: true}
     }))
+    .pipe(gulp.dest('./dist'));
+});
+
+// Minify JS
+gulp.task('scripts', function(){
+  gulp.src('./js/ginger.js')
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(uglify())
     .pipe(gulp.dest('./dist'));
 });
 
